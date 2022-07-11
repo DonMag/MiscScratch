8b3e49263eba1bb5c1b454d962aac3b97f2b8d00
DonMag <don_mail_2000@yahoo.com>
2022-07-10 22:05:49 -0400

    0710a

---
+++ CollectionStuff/CollectionStuff/MiscViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MiscViewController.swift
+//  CollectionStuff
+//
+//  Created by Don Mag on 7/10/22.
+//
+
+import UIKit
+
+class MiscViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
